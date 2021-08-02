74e1af6c634de2ab87dd0f198807d70e3f789574
kibukj <kibuzhj@gmail.com>
2021-08-02 16:02:48 -0400

    Add uiprompt dependency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,8 @@ fx_version 'adamant'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+dependency "uiprompt"
+
 files {
 	'ui/index.xhtml',
 	'ui/style.css',
